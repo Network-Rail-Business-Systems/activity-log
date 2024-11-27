84478f06ff1c3d6f411a07efe29a8260bdb84a82
Calum Gray <calum.gray@networkrail.co.uk>
2024-11-27 11:35:44 +0000

    made user factory

--- tests/Database/Factories/UserFactory.php
+++ tests/Database/Factories/UserFactory.php
@@ -9,9 +9,14 @@ abstract class UserFactory extends Factory {
 
     protected $model = User::class;
 
-    public function definition() {
+    public function definition(): array
+    {
         return [
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'username' => $this->faker->unique()->userName(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
 }
