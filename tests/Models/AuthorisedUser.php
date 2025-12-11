3c616bff51fc6b8efddfa7f716825625ad0dc194
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2025-12-11 10:40:42 +0000

    Updated badges

--- tests/Models/AuthorisedUser.php
+++ tests/Models/AuthorisedUser.php
@@ -2,8 +2,12 @@
 
 namespace NetworkRailBusinessSystems\ActivityLog\Tests\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class AuthorisedUser extends User
 {
+    use SoftDeletes;
+
     protected $table = 'users';
 
     public function permission(): string|false
